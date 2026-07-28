9447390652306272f9f3c53ed144b382fbc5384c
Lakshya Sharma <75852681+lakshya2851@users.noreply.github.com>
2026-07-28 21:06:49 +0530

    Time: 2 ms (10.75%), Space: 43.6 MB (33.83%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,33 +1,18 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> list = new ArrayList<>();
+        List<List<Integer>> pascal = new ArrayList<>();
 
-        if (numRows >= 1) {
-            list.add(new ArrayList<>(Arrays.asList(1)));
-        }
-        
-        if (numRows >= 2) {
-            list.add(new ArrayList<>(Arrays.asList(1, 1)));
-        }
-
-        for(int i=2; i<numRows; i++) {
-            List<Integer> temp = new ArrayList<>();
-            List<Integer> stored = list.get(list.size()-1);
-
-            for(int j=0; j<=i; j++) {
-                if(j == 0) {
-                    temp.add(stored.get(0));
-                }
-                else if(j == i) {
-                    temp.add(stored.get(stored.size()-1));
-                }
-                else {
-                    temp.add(stored.get(j-1) + stored.get(j));
-                }
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>(Collections.nCopies(i + 1, 1));
+            
+            for (int j = 1; j < i; j++) {
+                int val = pascal.get(i - 1).get(j - 1) + pascal.get(i - 1).get(j);
+                row.set(j, val);
             }
-            list.add(new ArrayList(temp));
+            pascal.add(row);
         }
-
-        return list;
+        return pascal;
     }
 }
